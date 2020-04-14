abb1a058b9d883b11a47eb8881c55cfd50cacc2f
dongyafei <670772351@qq.com>
2020-04-14 15:34:51 +0800

    增加封装driver

--- src/main/java/com/github/herowzz/atm/driver/WebChromeDriver.java
+++ src/main/java/com/github/herowzz/atm/driver/WebChromeDriver.java
@@ -2,22 +2,20 @@ package com.github.herowzz.atm.driver;
 
 import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 /**
  * Chrome驱动
  * @author wangzz
  */
-public class WebChromeDriver implements ITestDriver<WebDriver> {
+public class WebChromeDriver implements ITestDriver<AtmWebDriver> {
 
-	private WebDriver driver;
+	private AtmWebDriver driver;
 
 	public WebChromeDriver() {
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("start-maximized");
-		driver = new ChromeDriver(options);
+		driver = new AtmWebDriver(options);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);// 设置查询组件等待时间
 		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);// 设置页面加载超时
 		driver.manage().timeouts().setScriptTimeout(20, TimeUnit.SECONDS);
@@ -35,7 +33,7 @@ public class WebChromeDriver implements ITestDriver<WebDriver> {
 	}
 
 	@Override
-	public WebDriver getDriver() {
+	public AtmWebDriver getDriver() {
 		return driver;
 	}
 
